90aaca9d57be17469e57d3619498a5c552d32d47
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2025-05-25 09:32:44 +0700

    up

--- src/components/Template/index.jsx
+++ src/components/Template/index.jsx
@@ -14,67 +14,83 @@ import t12 from "../../assets/template/discography_home.jpg";
 import t13 from "../../assets/template/videos_home.jpg";
 import t14 from "../../assets/template/blog_home.jpg";
 import t15 from "../../assets/template/artist_metro.jpg";
+import { handleBlank } from "../../helpers/blank";
 
 const template = [
     {
         title: "Main Home",
         image: t1,
+        link: "https://preview.wolfthemes.store/herion/",
     },
     {
         title: "One-Page",
         image: t2,
+        link: "https://preview.wolfthemes.store/herion/home/one-page/",
     },
     {
         title: "Band Home",
         image: t3,
+        link: "https://preview.wolfthemes.store/herion/home/band-home/",
     },
     {
         title: "Home Alt",
         image: t4,
+        link: "https://preview.wolfthemes.store/herion/home/home-alt/",
     },
     {
         title: "Minimal",
         image: t5,
+        link: "https://preview.wolfthemes.store/herion/home/minimal/",
     },
     {
         title: "DJ/Event",
         image: t6,
+        link: "https://preview.wolfthemes.store/herion/home/dj-events/",
     },
     {
         title: "Festival",
         image: t7,
+        link: "https://preview.wolfthemes.store/herion/home/festival/",
     },
     {
         title: "Record Studio",
         image: t8,
+        link: "https://preview.wolfthemes.store/herion/home/recording-studio/",
     },
     {
         title: "Interactive Links",
         image: t9,
+        link: "https://preview.wolfthemes.store/herion/home/interactive-links/",
     },
     {
         title: "Artist Slider",
         image: t10,
+        link: "https://preview.wolfthemes.store/herion/home/artist-slider/",
     },
     {
         title: "Shop Home",
         image: t11,
+        link: "https://preview.wolfthemes.store/herion/home/shop-home/",
     },
     {
         title: "Discography Home",
         image: t12,
+        link: "https://preview.wolfthemes.store/herion/home/discography-home/",
     },
     {
         title: "Videos Home",
         image: t13,
+        link: "https://preview.wolfthemes.store/herion/home/videos-home/",
     },
     {
         title: "Blog Home",
         image: t14,
+        link: "https://preview.wolfthemes.store/herion/home/blog-home/",
     },
     {
         title: "Artist Metro",
         image: t15,
+        link: "https://preview.wolfthemes.store/herion/home/artists-home/",
     },
 ];
 
@@ -93,7 +109,11 @@ const Template = () => {
                     {template.map((item, index) => {
                         return (
                             <div className="card" key={index}>
-                                <img alt="template" src={item.image} />
+                                <img
+                                    alt="template"
+                                    src={item.image}
+                                    onClick={() => handleBlank(item.link)}
+                                />
                                 <p>{item.title}</p>
                             </div>
                         );
